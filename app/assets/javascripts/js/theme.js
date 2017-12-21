af55e38923197b19fded8a876578921c90f7ebf7
Maxime Bento <32094911+Ekineox@users.noreply.github.com>
2017-12-21 18:00:16 +0100

    Update theme.js

--- app/assets/javascripts/js/theme.js
+++ app/assets/javascripts/js/theme.js
@@ -20,14 +20,19 @@ $(function () {
 });
 
     // Animate.js Test
-var myAnimation = anime({
-  targets: ['.blue', '.green'],
-  translateX: '13rem',
-  rotate: 180,
-  borderRadius: '8px',
-  duration: 2000,
-  loop: true
+document.querySelector(".card-flip").classList.toggle("flip");
+
+/* 
+ * Holder.js for demo image
+ * Just for demo purpose
+ */
+Holder.addTheme('gray', {
+  bg: '#777',
+  fg: 'rgba(255,255,255,.75)',
+  font: 'Helvetica',
+  fontweight: 'normal'
 });
+
     // Animate.js Test
 
 
